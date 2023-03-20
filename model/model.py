10b20595a5610e26efa171511b8038635da74279
Saurabh Kishore <gks210199@gmail.com>
2023-03-20 13:17:54 -0400

    data imported

--- model/model.py
+++ model/model.py
@@ -1,6 +1,12 @@
-import tensorflow
+import tensorflow as tf
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
 import os 
 
-gpus = tensorflow.config.experimental.list_physical_devices('GPU')
+
+#The following two lines limits tensorflow from using all the gpus on our machine. 
+gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
-	tensorflow.config.experimental.set_memory_growth(gpu, True)
+	tf.config.experimental.set_memory_growth(gpu, True)
+
