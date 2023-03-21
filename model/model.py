674c63afa9b008047052bca11386448d9db911bb
Saurabh Kishore <gks210199@gmail.com>
2023-03-20 22:59:38 -0400

    changes to model.py

--- model/model.py
+++ model/model.py
@@ -1,12 +1,34 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib
-from matplotlib import pyplot as plt
 import os 
+from matplotlib import pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensroflow.keras.models import Conv2D, Maxpooling2D, Dense, Flatten, Dropout
 
 
-#The following two lines limits tensorflow from using all the gpus on our machine. 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-	tf.config.experimental.set_memory_growth(gpu, True)
+model = Sequential()
 
+#Adding a convulutional layer and a maxpooling layer to the model
+
+
+#Layer 1 
+	#Conv2D parameters, 16 filters, 3x3 pixels with stride length 1 with the Relu activation with specified input size
+	#Then use the maxpooling layer to get the maxvalue of a 2x2 region for each region 
+model.add(Conv2D(16, (3,3),1, activation='relu', input_shape=(224,224,3)))
+model.add(Maxpooling2D())
+
+#Layer 2
+model.add(Conv2D(32, (3,3), 1, activation='relu'))
+model.add(Maxpooling2D())
+
+#Layer 3
+model.add(Conv2D(16, (3,3), 1, activation='relu'))
+model.add(Maxpooling2D())
+
+#Layer 4
+model.add(Flatten())
+
+#Layer 5
+model.add(Dense(256, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
